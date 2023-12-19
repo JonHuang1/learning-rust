1932be6fd830eaad62da71687822de251425983a
JonHuang1 <jonhuang918@gmail.com>
2023-12-19 18:45:27 -0500

    pre cargo fix

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -6,6 +6,21 @@ fn main() {
     another_function2(5);
 
     print_labeled_measurement(5, 'h');
+
+    let y0 = 6;
+
+    /*
+     This will cause error because "let y = 6" does not return a value
+     so x has nothing to bind to
+    */
+    // let x = (let y = 6);
+
+    let y1 = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y1 is: {y1}");
+
 }
 
 fn another_function() {
